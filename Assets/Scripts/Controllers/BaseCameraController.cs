fec7ff38ce9c5f1ea6f637fba1d13f919ca5793e
Lawrence Lopez <lawgnut@gmail.com>
2016-02-28 10:48:04 -0800

    Added Simple Colors folder for testing. BaseController.cs refactored to BaseCharacterController.cs Added BaseCameraController.cs
    BaseCharacterController and BaseCameraController work. The monster moves forward and backwards, and rotates left and right. The camera follows the monster from behind, at an offset.

---
+++ Assets/Scripts/Controllers/BaseCameraController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseCameraController : MonoBehaviour 
+{
+	public Transform target;
+	public float lookSmooth = 0.09f;
+	public Vector3 offsetFromTarget = new Vector3(0,6,-8);
+	public float xTilt = 10;
+
+	Vector3 destination = Vector3.zero;
+	BaseCharacterController charController;
+	float rotateVel = 0;
+
+	void Start () 
+	{
+		SetCameraTarget(target);
+	}
+
+	void SetCameraTarget(Transform t)
+	{
+		target = t;
+
+		if (target != null)
+		{
+			if (target.GetComponent<BaseCharacterController>())
+			{
+				charController = target.GetComponent<BaseCharacterController>();
+			}
+			else
+			{
+				Debug.LogError("The camera's target needs a character controller.");
+			}
+		}
+		else
+		{
+			Debug.LogError("Your camera needs a target.");
+		}
+
+	}
+	
+	void LateUpdate () 
+	{
+		//moving
+		MoveToTarget();
+		//rotating
+		LookAtTarget();
+	}
+
+	void MoveToTarget()
+	{
+		destination = charController.TargetRotation * offsetFromTarget;
+		destination += target.position;
+		transform.position = destination;
+	}
+
+	void LookAtTarget()
+	{
+		float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateVel, lookSmooth);
+		transform.rotation = Quaternion.Euler(xTilt, eulerYAngle, 0);
+	}
+
+}
---
+++ Assets/Scripts/Controllers/BaseCharacterController.cs
@@ -0,0 +1,77 @@
+﻿// Lawrence Lopez
+// https://www.youtube.com/watch?v=BBS2nIKzmbw (Thank you Renaissance Coders)
+
+using UnityEngine;
+using System.Collections;
+
+public class BaseCharacterController : MonoBehaviour 
+{
+	public float inputDelay = 0.1f;
+	public float forwardVel = 12;
+	public float rotateVel = 100;
+
+	Quaternion targetRotation;
+	Rigidbody rBody;
+	float forwardInput, turnInput;
+
+	public Quaternion TargetRotation
+	{
+		get { return targetRotation;}
+	}
+
+	void Start () 
+	{
+		targetRotation = transform.rotation;
+		if (GetComponent<Rigidbody> ()) 
+		{
+			rBody = GetComponent<Rigidbody> ();
+		} 
+		else 
+		{
+			Debug.LogError ("This component requires a Rigidbody.");
+		}
+
+		forwardInput = turnInput = 0;
+	}
+
+	void GetInput()
+	{
+		forwardInput = Input.GetAxis("Vertical");
+		turnInput = Input.GetAxis("Horizontal");
+	}
+	
+	void Update () 
+	{
+		GetInput();
+		Turn();
+	}
+
+	void FixedUpdate()
+	{
+		Run();
+	}
+
+	void Run()
+	{
+		if (Mathf.Abs(forwardInput) > inputDelay)
+		{
+			// move
+			rBody.velocity = transform.forward * forwardInput * forwardVel;
+		}
+		else
+		{
+			// zero velocity
+			rBody.velocity = Vector3.zero;
+		}
+	}
+
+	void Turn()
+	{
+		if (Mathf.Abs(turnInput) > inputDelay)
+		{
+			targetRotation *= Quaternion.AngleAxis(rotateVel * turnInput * Time.deltaTime, Vector3.up);
+		}
+			transform.rotation = targetRotation;
+	}
+
+}
