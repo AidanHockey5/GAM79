8784b9b1732e6f082f868fd133663d1e8272cdba
Deven Fafard <deven.marie.fafard@gmail.com>
2016-03-25 13:02:47 -0700

    Implemented basic state machine template.
    Created enums and executors for each of the 4 classes and created a state machine manager object in lobby scene. Currently using NONE, IDLE, MOVING, ATTACKING, and DEAD states for all classes. No functionality written into the states yet.

---
+++ Assets/Scripts/Enums/MechState.cs
@@ -0,0 +1,9 @@
+﻿public enum MechState
+{
+	NONE,
+	IDLE,
+	MOVING,
+	ATTACKING,
+	DEAD
+}
+
---
+++ Assets/Scripts/Game Logic/State Machine Executors/MechStateExecutor.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MechStateExecutor : MonoBehaviour 
+{
+	public List<MechState> mechStates = new List<MechState> ();
+	StateMachineEngine stateMachine = null;
+
+	#region MONOBEHAVIORS
+	void Awake()
+	{
+		//Looks for state machine engine.
+		stateMachine = GameObject.FindObjectOfType<StateMachineEngine> () as StateMachineEngine;
+	}
+
+	void Start () 
+	{
+		//Associates object to selected state enum then changes state to default.
+		stateMachine.Initialize<MechState> (this);
+		stateMachine.ChangeState (MechState.NONE);
+	}
+
+	void Update () 
+	{
+		//Gets lists of all states in enum.
+		mechStates = stateMachine.GetStates<MechState> ();
+	}
+	#endregion
+
+	#region NONE
+	public void NONE_Enter()
+	{
+		//Runs when object enters state.
+		Debug.Log(this.name + " : No state is selected!");
+	}
+
+	public void NONE_Update()
+	{
+		//Works just like regular update, but only runs while the state is active.
+	}
+
+	public void NONE_Exit()
+	{
+		//Runs when object exits state.
+	}
+	#endregion
+
+	#region IDLE
+	public void IDLE_Enter()
+	{
+
+	}
+
+	public void IDLE_Update()
+	{
+
+	}
+
+	public void IDLE_Exit()
+	{
+
+	}
+	#endregion
+
+	#region MOVING
+	public void MOVING_Enter()
+	{
+
+	}
+
+	public void MOVING_Update()
+	{
+
+	}
+
+	public void MOVING_Exit()
+	{
+
+	}
+	#endregion
+
+	#region ATTACKING
+	public void ATTACKING_Enter()
+	{
+
+	}
+
+	public void ATTACKING_Update()
+	{
+
+	}
+
+	public void ATTACKING_Exit()
+	{
+
+	}
+	#endregion
+
+	#region DEAD
+	public void DEAD_Enter()
+	{
+
+	}
+
+	public void DEAD_Update()
+	{
+
+	}
+
+	public void DEAD_Exit()
+	{
+
+	}
+	#endregion
+}
