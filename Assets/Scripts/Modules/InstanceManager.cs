47d53c8e2359ac21bc8a8fe9418a43e0280aeeac
Chris Chance <chance@brokencirclestudios.com>
2016-03-25 17:27:16 -0700

    working on singleton manager

---
+++ Assets/Scripts/Modules/InstanceManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+public class InstanceManager 
+{
+	public static T GetInstance<T> () where T : class
+	{
+		return InstanceManager.InstanceContainer<T>.Instance as T;	
+	}
+
+	public static void Register<T> (T instance) where T : class
+	{
+		InstanceManager.InstanceContainer<T>.Instance = instance;
+	}
+
+	public static void Unregister<T> () where T : class
+	{
+		InstanceManager.InstanceContainer<T>.Instance = (T)((object)null);
+	}
+
+	private class InstanceContainer<T> where T : class
+	{
+		private static T _instance;
+		private static 
+
+		public static T Instance
+		{
+			get 
+			{
+				T instance = InstanceManager.InstanceContainer<T>._instance;
+				T result;
+				if (instance != null) 
+				{
+					result = instance;
+				}
+				else 
+				{
+					result = (InstanceManager.InstanceContainer<T>._instance = Activator.CreateInstance<T>());
+				}
+				return result;
+			}
+			set
+			{
+				InstanceManager.InstanceContainer<T>._instance = value;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Modules/SingletonCreator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class SingletonCreator : MonoBehaviour 
+{
+	void Awake () 
+	{
+		CreateSingletons ();
+	}
+	
+	private void CreateSingletons()
+	{
+		var singletonTypes = new List<Type>();
+		foreach(var candidate in GetType().Assembly.GetExportedTypes())
+		{
+			var attributes = candidate.GetCustomAttributes(typeof(Singleton), true);
+			if (attributes != null && attributes.Length > 0)
+			{
+				singletonTypes.Add(candidate);
+			}
+		}
+		new GameObject("Singletons", singletonTypes.ToArray());
+	}
+}
