028e08544f06e6b6472b75039ec8f3ee71e7a89d
Ryan Zagala <gman10501@gmail.com>
2016-03-31 22:17:13 -0700

    Local Health Display is deactived for local player while other players can view the that players local health display. Created simple objective markers using materials and cubes for later use when we have objectives. Need to test health damage again to test HUD health bar and local health bar.

--- Assets/Scripts/UI/LocalUIDisplay.cs
+++ Assets/Scripts/UI/LocalUIDisplay.cs
@@ -1,18 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
+using System.Collections.Generic;
 
-public class LocalUIDisplay : MonoBehaviour
+public class LocalUIDisplay : NetworkBehaviour
 {
+    public GameObject localHealthDisplay;
 
 	// Use this for initialization
 	void Start ()
     {
-	
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.LookAt(Camera.main.transform.position);
-	}
+        if (gameObject.tag == "HumanHP" && gameObject.GetComponentInParent<NetworkIdentity>().isLocalPlayer)
+        {
+            localHealthDisplay.SetActive(false);                                                // Makes Local Health Bar invisible only for local player
+            if (GameObject.FindGameObjectWithTag("Monster") == null)
+            {
+                return;
+            }
+            transform.LookAt(GameObject.FindGameObjectWithTag("Monster").transform.position);   // Will Find Monster and other Human Cameras Later
+        }
+        else if (gameObject.tag == "MonsterHP" && gameObject.GetComponentInParent<NetworkIdentity>().isLocalPlayer)
+        {
+            localHealthDisplay.SetActive(false);
+            if (GameObject.FindGameObjectWithTag("Human") == null)
+            {
+                return;
+            }
+            transform.LookAt(GameObject.FindGameObjectWithTag("Human").transform.position);
+        }
+    }
 }
