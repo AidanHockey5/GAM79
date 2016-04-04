08d137480c43a5d0a1b6609f34b57ee37752c5b4
Lewis Griffith <griffymp@yahoo.com>
2016-04-04 08:14:47 -0700

    SpawnSystem Rewrite 1.1

--- Assets/Scripts/Game Logic/SpawnPointManager.cs
+++ Assets/Scripts/Game Logic/SpawnPointManager.cs
@@ -5,11 +5,12 @@ using System.Collections.Generic;
 [Singleton]
 public class SpawnPointManager : MonoBehaviour
 {
-   
-
     public List<SpawnPoints> spawnPointObject = new List<SpawnPoints>();
 
-   
+    public Transform player;
+
+    float monstDistance = 0.0f;
+    float pointDistance = 0.0f;
 
     void Awake()
     {
@@ -19,14 +20,42 @@ public class SpawnPointManager : MonoBehaviour
    
 	void Start () 
     {
-     
+        player = gameObject.GetComponent<BaseCharacterController>().transform;
 	}
     void Update()
     {
-        
+        foreach (SpawnPoints item in spawnPointObject)
+        {
+            spawnPointObject.Add(item);
+
+           /* if (item != null)
+            {
+                
+            }*/
+        }
+    }
+
+    public void RegisterMonsterDistance()
+    {
+        monstDistance = Vector3.Distance(gameObject.GetComponent<MonsterDistance>().transform.position, player.position);
+    }
+
+    public void RegisterSpawnPoints()
+    {
+        pointDistance = Vector3.Distance((gameObject.GetComponent<SpawnPoints>().transform.position), gameObject.GetComponent<MonsterDistance>().transform.position);
+    }
+
+    public void RegisterGameManager()
+    {
+
     }
 
     public void PlayerRebirth(GameObject player)
+    {
+        player.transform.position = new Vector3(0, 0, 0);
+    }
+
+    public void OnTriggerEnter(Collider other)
     {
 
     }
