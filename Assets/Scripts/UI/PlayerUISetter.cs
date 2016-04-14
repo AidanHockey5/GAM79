bbe42d1bb16c0369a2eecf4a221bc7048292e642
Ryan Zagala <gman10501@gmail.com>
2016-04-13 20:25:10 -0700

    Combined Design teams Monster and Technician HUDs with this branch.

--- Assets/Scripts/UI/PlayerUISetter.cs
+++ Assets/Scripts/UI/PlayerUISetter.cs
@@ -58,26 +58,16 @@ public class PlayerUISetter : NetworkBehaviour
 
     public void HUDEditor()
     {
-        // Element 0 is Human Player's Health Text and Health Bar
-        // Element 1 is Ammo
-        // Element 2 is Ability 1
-        // Element 3 is Ability 2
-        // Element 4 is Monster Player's Health Text and Health Bar
-        // Element 5 is Attack Stamina
+        // Element 0 is Technician Human Player HUD
+        // Element 1 is Monster Player HUD
 
         if (playerType == "Human" && gameObject.GetComponent<NetworkIdentity>().localPlayerAuthority == true)
         {
-            for (int count = 0; count < 4; count++)
-            {
-                HUDUISetter.HUDStuff[count].SetActive(true);
-            }
+            HUDUISetter.HUDStuff[0].SetActive(true);
         }
         else if (playerType == "Monster" && gameObject.GetComponent<NetworkIdentity>().localPlayerAuthority == true)
         {
-            for (int count = 4; count < 6; count++)
-            {
-                HUDUISetter.HUDStuff[count].SetActive(true);
-            }
+            HUDUISetter.HUDStuff[1].SetActive(true);
         }
 
 		if (localHealthDisplay == null)
@@ -105,14 +95,14 @@ public class PlayerUISetter : NetworkBehaviour
     {
         if (gameObject.tag == "Human")
         {
-            healthBar = gManager.humanHealthBar;                                                        // Utilizes the humanHealthBar RectTransform in GameManager.cs
-            healthBar.sizeDelta = new Vector2(health * 20, healthBar.sizeDelta.y);                      // rectTransform.sizeDelta.x always equals 200
+            healthBar = gManager.humanHealthBar;                                                                            // Utilizes the humanHealthBar RectTransform in GameManager.cs
+            healthBar.sizeDelta = new Vector2((1570.9f / maxHealthUI) * health, healthBar.sizeDelta.y);                     // Use print(healthBar.sizeDelta) to get the x-value size of healthbar to do the math          
             localHealthBar.sizeDelta = new Vector2(health * 20, localHealthBar.sizeDelta.y);
         }
         else if (gameObject.tag == "Monster")
         {
-            healthBar = gManager.monsterHealthBar;                                                      // Utilizes the monsterHealthBar RectTransform in GameManager.cs
-            healthBar.sizeDelta = new Vector2(health * 2, healthBar.sizeDelta.y);
+            healthBar = gManager.monsterHealthBar;                                                                          // Utilizes the monsterHealthBar RectTransform in GameManager.cs
+            healthBar.sizeDelta = new Vector2((1423.0f / maxHealthUI) * health, healthBar.sizeDelta.y);                     // Use print(healthBar.sizeDelta) to get the x-value size of healthbar to do the math
             localHealthBar.sizeDelta = new Vector2(health * 2, localHealthBar.sizeDelta.y);
         }
     }
