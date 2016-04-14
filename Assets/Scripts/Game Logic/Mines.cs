9a566b4beec097bebcf9d215eb48e04212e6ab39
Lewis Griffith <griffymp@yahoo.com>
2016-04-13 23:31:52 -0700

    Mines instantiate to a single point but have no functionality that will be done tomorrow

---
+++ Assets/Scripts/Game Logic/Mines.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+[RequireComponent(typeof(NetworkTransform))]
+public class Mines : NetworkBehaviour
+{
+    public GameObject minePrefab;
+    // Use this for initialization
+    [ServerCallback]
+	void Start () 
+    {
+        if (minePrefab == null)
+        {
+            minePrefab = (GameObject)(UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Mine.fbx", typeof(GameObject)));
+        }
+	}
+	
+	// Update is called once per frame
+   [Server]
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+           MineCreated();
+        }
+	}
+    [Server]
+    public void MineCreated()
+    {
+        Instantiate(minePrefab, transform.position, Quaternion.identity);
+        
+    }
+   
+}
