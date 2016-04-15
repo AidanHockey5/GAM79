88b5a2456a1dc788d42a9d6d4c0d3cb609c8e6a9
Lewis Griffith <griffymp@yahoo.com>
2016-04-14 19:47:01 -0700

    Network errors

---
+++ Assets/Scripts/Game Logic/Mine.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Mine : NetworkBehaviour
+{
+  
+    // Use this for initialization
+
+	void Start () 
+    {
+      
+	}
+	
+	// Update is called once per frame
+  
+	void Update () 
+    {
+        
+	}
+   
+   
+}
---
+++ Assets/Scripts/Game Logic/MineSpawning.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+[RequireComponent(typeof(NetworkTransform))]
+public class MineSpawning : NetworkBehaviour 
+{
+    public GameObject minePrefab;
+    public Transform spawnPoint;
+
+    // Use this for initialization
+    [Client]
+    void Start()
+    {
+        if (minePrefab == null)
+        {
+            minePrefab = (GameObject)(UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Mine.fbx", typeof(GameObject)));
+           
+        }
+    }
+
+    // Update is called once per frame
+    [Client]
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+
+            CmdSpawnMine(minePrefab, GetComponent<MineSpawner>().transform);
+        }
+    }
+    [Command]
+    private void CmdSpawnMine(GameObject minePrefab, Transform spawnPoint)
+    {
+        GameObject mine = (GameObject)Instantiate(minePrefab, spawnPoint.position, spawnPoint.rotation);
+        NetworkServer.Spawn(mine);
+    }
+}
