471f1a4f389ca0d159a64fbd439dd6fd6b48a821
Chris Chance <chance@brokencirclestudios.com>
2016-04-16 00:54:09 -0700

    -More work on CSV parser

---
+++ Assets/Scripts/Misc/CSVParser.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class CSVParser : MonoBehaviour 
+{
+	static string[] playerData;
+	static string[] monsterData;
+
+	[SerializeField]
+	private TextAsset PlayerCSV, MonsterCSV;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerData = ParseData (PlayerCSV);
+		monsterData = ParseData (MonsterCSV);
+	
+	}
+
+	static string[] ParseData(TextAsset asset)
+	{
+		char[] delimiters = new char[] { ',' };
+		string fileText = asset.text;
+		string[] lines = fileText.Split('\n');
+		List<string> strings = new List<string> ();
+		for(int i = 0; i < lines.Length; i++)
+		{
+			string[] parts = lines[i].Split (delimiters);
+			foreach (var part in parts)
+			{
+				strings.Add (part);
+			}
+		}
+
+		return strings.ToArray();
+	}
+}
